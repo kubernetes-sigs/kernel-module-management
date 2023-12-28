a6444de00fd9911dcb08dfa42e01ff28aaf23b0f
Yevgeny Shnaidman <60741237+yevgeny-shnaidman@users.noreply.github.com>
2023-12-28 11:31:39 +0200

    Refactor worker to support different type of image to be mounted (#677)
    Currently worker only supports remote image mounting (from a repo).
    In the future we might want to support also mounting an image from
    a local tar file. This Pr lays a foundation for that. The basic idea
    is that all the code should stay the same, and only the image mounting
    implementation should change, as well as adding additional cobra
    commands. The sequence to add a new image mounter option is:
    1. Define a new cobra command and mount it under root command
    2. the PreRun command in the new command should initialize the worker
       with the new image mounter
    3. define a new load/unload cobra commands that should be exact replicas
       of the existing load/unload cobra command. The copy must be done due
       to the fact that in cobra a command cannot be a child of multiple
       commands
    Changes in the PR:
    1. kmod cobra command now initializes the worker. it will initiaze it
       with remote image mounter implementation. In case we need to add ad
       different mounter, we just define a different kmod cobra command and
       it will initialize the worker with the different image mounter
       implmentation
    2. Change ImagePuller interface to ImageMounter interface
    3. Move ImageMounter interface definition to a dedicated file
    4. Move the implementation of the RemoteImageMounter into the dedicated
       file
    5 update unit-tests

--- cmd/worker/funcs_kmod.go
+++ cmd/worker/funcs_kmod.go
@@ -7,6 +7,25 @@ import (
 	"github.com/spf13/cobra"
 )
 
+func kmodFuncPreRunE(cmd *cobra.Command, args []string) error {
+	err := cmd.Parent().PersistentPreRunE(cmd.Parent(), args)
+	if err != nil {
+		return fmt.Errorf("failed to call root command pre-run: %v", err)
+	}
+
+	logger.Info("Reading pull secrets", "base dir", worker.PullSecretsDir)
+	keyChain, err := worker.ReadKubernetesSecrets(cmd.Context(), worker.PullSecretsDir, logger)
+	if err != nil {
+		return fmt.Errorf("could not read pull secrets: %v", err)
+	}
+
+	ip := worker.NewRemoteImageMounter(worker.ImagesDir, keyChain, logger)
+	mr := worker.NewModprobeRunner(logger)
+	w = worker.NewWorker(ip, mr, logger)
+
+	return nil
+}
+
 func kmodLoadFunc(cmd *cobra.Command, args []string) error {
 	cfgPath := args[0]
 
@@ -28,4 +47,19 @@ func kmodLoadFunc(cmd *cobra.Command, args []string) error {
 	mountPathFlag := cmd.Flags().Lookup(worker.FlagFirmwareMountPath)
 
 	return w.LoadKmod(cmd.Context(), cfg, mountPathFlag.Value.String())
+}
+
+func kmodUnloadFunc(cmd *cobra.Command, args []string) error {
+	cfgPath := args[0]
+
+	logger.V(1).Info("Reading config", "path", cfgPath)
+
+	cfg, err := configHelper.ReadConfigFile(cfgPath)
+	if err != nil {
+		return fmt.Errorf("could not read config file %s: %v", cfgPath, err)
+	}
+
+	mountPathFlag := cmd.Flags().Lookup(worker.FlagFirmwareMountPath)
+
+	return w.UnloadKmod(cmd.Context(), cfg, mountPathFlag.Value.String())
 }
---
+++ internal/worker/imagemounter.go
@@ -0,0 +1,13 @@
+package worker
+
+import (
+	"context"
+
+	kmmv1beta1 "github.com/kubernetes-sigs/kernel-module-management/api/v1beta1"
+)
+
+//go:generate mockgen -source=imagemounter.go -package=worker -destination=mock_imagemounter.go
+
+type ImageMounter interface {
+	MountImage(ctx context.Context, imageName string, cfg *kmmv1beta1.ModuleConfig) (string, error)
+}
