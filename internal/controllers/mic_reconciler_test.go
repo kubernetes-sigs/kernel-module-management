6f154bd384c741a94e4e2400dc69a23852f38d6a
Yoni Bettan <yonibettan@gmail.com>
2025-02-10 11:32:59 +0200

    Creating the `pod` package.
    The MIC controller is defining an helper interface for manipulating
    pods. The NMC controller is doing the same. Moving the MIC's pod interface
    to its own package for simplicity and code re-use.
    
    The NMC's pod interface will move to this package as well in an upcoming
    commit.
    
    Signed-off-by: Yoni Bettan <yonibettan@gmail.com>

--- internal/controllers/mic_reconciler_test.go
+++ internal/controllers/mic_reconciler_test.go
@@ -4,38 +4,36 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"reflect"
 
-	"github.com/google/go-cmp/cmp"
 	kmmv1beta1 "github.com/kubernetes-sigs/kernel-module-management/api/v1beta1"
 	"github.com/kubernetes-sigs/kernel-module-management/internal/client"
 	"github.com/kubernetes-sigs/kernel-module-management/internal/mbsc"
 	"github.com/kubernetes-sigs/kernel-module-management/internal/mic"
+	"github.com/kubernetes-sigs/kernel-module-management/internal/pod"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"go.uber.org/mock/gomock"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	ctrlclient "sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
 var _ = Describe("MicReconciler_Reconcile", func() {
 	var (
 		ctrl               *gomock.Controller
-		mockPodHelper      *MockpullPodManager
+		mockImagePuller    *pod.MockImagePuller
 		mockMicReconHelper *MockmicReconcilerHelper
 		mr                 *micReconciler
 	)
 
 	BeforeEach(func() {
 		ctrl = gomock.NewController(GinkgoT())
-		mockPodHelper = NewMockpullPodManager(ctrl)
+		mockImagePuller = pod.NewMockImagePuller(ctrl)
 		mockMicReconHelper = NewMockmicReconcilerHelper(ctrl)
 
 		mr = &micReconciler{
 			micReconHelper: mockMicReconHelper,
-			podHelper:      mockPodHelper,
+			imagePullerAPI: mockImagePuller,
 		}
 	})
 
@@ -51,10 +49,10 @@ var _ = Describe("MicReconciler_Reconcile", func() {
 		expectedErr := returnedError
 		pullPods := []v1.Pod{}
 		if listPullPodsError {
-			mockPodHelper.EXPECT().listImagesPullPods(ctx, &testMic).Return(nil, returnedError)
+			mockImagePuller.EXPECT().ListPullPods(ctx, &testMic).Return(nil, returnedError)
 			goto executeTestFunction
 		}
-		mockPodHelper.EXPECT().listImagesPullPods(ctx, &testMic).Return(pullPods, nil)
+		mockImagePuller.EXPECT().ListPullPods(ctx, &testMic).Return(pullPods, nil)
 		if updateStatusByPodsError {
 			mockMicReconHelper.EXPECT().updateStatusByPullPods(ctx, &testMic, pullPods).Return(returnedError)
 			goto executeTestFunction
@@ -92,21 +90,21 @@ var _ = Describe("MicReconciler_Reconcile", func() {
 
 var _ = Describe("updateStatusByPullPods", func() {
 	var (
-		ctrl          *gomock.Controller
-		clnt          *client.MockClient
-		statusWriter  *client.MockStatusWriter
-		mockPodHelper *MockpullPodManager
-		micHelper     *mic.MockMIC
-		mrh           micReconcilerHelper
+		ctrl            *gomock.Controller
+		clnt            *client.MockClient
+		statusWriter    *client.MockStatusWriter
+		mockImagePuller *pod.MockImagePuller
+		micHelper       *mic.MockMIC
+		mrh             micReconcilerHelper
 	)
 
 	BeforeEach(func() {
 		ctrl = gomock.NewController(GinkgoT())
 		clnt = client.NewMockClient(ctrl)
 		statusWriter = client.NewMockStatusWriter(ctrl)
-		mockPodHelper = NewMockpullPodManager(ctrl)
+		mockImagePuller = pod.NewMockImagePuller(ctrl)
 		micHelper = mic.NewMockMIC(ctrl)
-		mrh = newMICReconcilerHelper(clnt, mockPodHelper, micHelper, nil, nil)
+		mrh = newMICReconcilerHelper(clnt, mockImagePuller, micHelper, nil, nil)
 	})
 
 	ctx := context.Background()
@@ -137,16 +135,13 @@ var _ = Describe("updateStatusByPullPods", func() {
 	})
 
 	It("pod's image is not in spec", func() {
-		pullPod := v1.Pod{
-			ObjectMeta: metav1.ObjectMeta{
-				Labels: map[string]string{imageLabelKey: "image 3"},
-			},
-		}
+		pullPod := v1.Pod{}
 		gomock.InOrder(
+			mockImagePuller.EXPECT().GetPullPodImage(pullPod).Return("image 3"),
 			micHelper.EXPECT().GetModuleImageSpec(&testMic, "image 3").Return(nil),
 			clnt.EXPECT().Status().Return(statusWriter),
 			statusWriter.EXPECT().Patch(ctx, &testMic, gomock.Any()),
-			mockPodHelper.EXPECT().deletePod(ctx, &pullPod).Return(nil),
+			mockImagePuller.EXPECT().DeletePod(ctx, &pullPod).Return(nil),
 		)
 		err := mrh.updateStatusByPullPods(ctx, &testMic, []v1.Pod{pullPod})
 		Expect(err).To(BeNil())
@@ -154,19 +149,17 @@ var _ = Describe("updateStatusByPullPods", func() {
 
 	It("pod failed, build config present", func() {
 		pullPod := v1.Pod{
-			ObjectMeta: metav1.ObjectMeta{
-				Labels: map[string]string{imageLabelKey: "image 1"},
-			},
 			Status: v1.PodStatus{
 				Phase: v1.PodFailed,
 			},
 		}
 		gomock.InOrder(
+			mockImagePuller.EXPECT().GetPullPodImage(pullPod).Return("image 1"),
 			micHelper.EXPECT().GetModuleImageSpec(&testMic, "image 1").Return(&testMic.Spec.Images[0]),
 			micHelper.EXPECT().SetImageStatus(&testMic, "image 1", kmmv1beta1.ImageNeedsBuilding),
 			clnt.EXPECT().Status().Return(statusWriter),
 			statusWriter.EXPECT().Patch(ctx, &testMic, gomock.Any()),
-			mockPodHelper.EXPECT().deletePod(ctx, &pullPod).Return(nil),
+			mockImagePuller.EXPECT().DeletePod(ctx, &pullPod).Return(nil),
 		)
 		err := mrh.updateStatusByPullPods(ctx, &testMic, []v1.Pod{pullPod})
 		Expect(err).To(BeNil())
@@ -174,19 +167,17 @@ var _ = Describe("updateStatusByPullPods", func() {
 
 	It("pod failed, build config not present, sign config present", func() {
 		pullPod := v1.Pod{
-			ObjectMeta: metav1.ObjectMeta{
-				Labels: map[string]string{imageLabelKey: "image 2"},
-			},
 			Status: v1.PodStatus{
 				Phase: v1.PodFailed,
 			},
 		}
 		gomock.InOrder(
+			mockImagePuller.EXPECT().GetPullPodImage(pullPod).Return("image 2"),
 			micHelper.EXPECT().GetModuleImageSpec(&testMic, "image 2").Return(&testMic.Spec.Images[1]),
 			micHelper.EXPECT().SetImageStatus(&testMic, "image 2", kmmv1beta1.ImageNeedsSigning),
 			clnt.EXPECT().Status().Return(statusWriter),
 			statusWriter.EXPECT().Patch(ctx, &testMic, gomock.Any()),
-			mockPodHelper.EXPECT().deletePod(ctx, &pullPod).Return(nil),
+			mockImagePuller.EXPECT().DeletePod(ctx, &pullPod).Return(nil),
 		)
 		err := mrh.updateStatusByPullPods(ctx, &testMic, []v1.Pod{pullPod})
 		Expect(err).To(BeNil())
@@ -194,18 +185,16 @@ var _ = Describe("updateStatusByPullPods", func() {
 
 	It("pod failed, build or sign config is not present", func() {
 		pullPod := v1.Pod{
-			ObjectMeta: metav1.ObjectMeta{
-				Labels: map[string]string{imageLabelKey: "image 3"},
-			},
 			Status: v1.PodStatus{
 				Phase: v1.PodFailed,
 			},
 		}
 		gomock.InOrder(
+			mockImagePuller.EXPECT().GetPullPodImage(pullPod).Return("image 3"),
 			micHelper.EXPECT().GetModuleImageSpec(&testMic, "image 3").Return(&testMic.Spec.Images[2]),
 			clnt.EXPECT().Status().Return(statusWriter),
 			statusWriter.EXPECT().Patch(ctx, &testMic, gomock.Any()),
-			mockPodHelper.EXPECT().deletePod(ctx, &pullPod).Return(nil),
+			mockImagePuller.EXPECT().DeletePod(ctx, &pullPod).Return(nil),
 		)
 		err := mrh.updateStatusByPullPods(ctx, &testMic, []v1.Pod{pullPod})
 		Expect(err).To(BeNil())
@@ -213,19 +202,17 @@ var _ = Describe("updateStatusByPullPods", func() {
 
 	It("pod succeeded", func() {
 		pullPod := v1.Pod{
-			ObjectMeta: metav1.ObjectMeta{
-				Labels: map[string]string{imageLabelKey: "image 2"},
-			},
 			Status: v1.PodStatus{
 				Phase: v1.PodSucceeded,
 			},
 		}
 		gomock.InOrder(
+			mockImagePuller.EXPECT().GetPullPodImage(pullPod).Return("image 2"),
 			micHelper.EXPECT().GetModuleImageSpec(&testMic, "image 2").Return(&testMic.Spec.Images[1]),
 			micHelper.EXPECT().SetImageStatus(&testMic, "image 2", kmmv1beta1.ImageExists),
 			clnt.EXPECT().Status().Return(statusWriter),
 			statusWriter.EXPECT().Patch(ctx, &testMic, gomock.Any()),
-			mockPodHelper.EXPECT().deletePod(ctx, &pullPod).Return(nil),
+			mockImagePuller.EXPECT().DeletePod(ctx, &pullPod).Return(nil),
 		)
 		err := mrh.updateStatusByPullPods(ctx, &testMic, []v1.Pod{pullPod})
 		Expect(err).To(BeNil())
@@ -341,22 +328,22 @@ var _ = Describe("updateStatusByMBSC", func() {
 
 var _ = Describe("processImagesSpecs", func() {
 	var (
-		ctrl          *gomock.Controller
-		clnt          *client.MockClient
-		mockPodHelper *MockpullPodManager
-		mbscHelper    *mbsc.MockMBSC
-		micHelper     *mic.MockMIC
-		mrh           micReconcilerHelper
-		testMic       kmmv1beta1.ModuleImagesConfig
+		ctrl            *gomock.Controller
+		clnt            *client.MockClient
+		mockImagePuller *pod.MockImagePuller
+		mbscHelper      *mbsc.MockMBSC
+		micHelper       *mic.MockMIC
+		mrh             micReconcilerHelper
+		testMic         kmmv1beta1.ModuleImagesConfig
 	)
 
 	BeforeEach(func() {
 		ctrl = gomock.NewController(GinkgoT())
 		clnt = client.NewMockClient(ctrl)
-		mockPodHelper = NewMockpullPodManager(ctrl)
+		mockImagePuller = pod.NewMockImagePuller(ctrl)
 		micHelper = mic.NewMockMIC(ctrl)
 		mbscHelper = mbsc.NewMockMBSC(ctrl)
-		mrh = newMICReconcilerHelper(clnt, mockPodHelper, micHelper, mbscHelper, scheme)
+		mrh = newMICReconcilerHelper(clnt, mockImagePuller, micHelper, mbscHelper, scheme)
 		testMic = kmmv1beta1.ModuleImagesConfig{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      "some name",
@@ -389,8 +376,8 @@ var _ = Describe("processImagesSpecs", func() {
 	It("image status empty, pull pod does not exists, need to create a pull pod", func() {
 		gomock.InOrder(
 			micHelper.EXPECT().GetImageState(&testMic, "image 1").Return(kmmv1beta1.ImageState("")),
-			mockPodHelper.EXPECT().getPullPodForImage(pullPods, "image 1").Return(nil),
-			mockPodHelper.EXPECT().createPullPod(ctx, &testMic.Spec.Images[0], &testMic).Return(nil),
+			mockImagePuller.EXPECT().GetPullPodForImage(pullPods, "image 1").Return(nil),
+			mockImagePuller.EXPECT().CreatePullPod(ctx, &testMic.Spec.Images[0], &testMic).Return(nil),
 		)
 		err := mrh.processImagesSpecs(ctx, &testMic, pullPods)
 		Expect(err).To(BeNil())
@@ -399,7 +386,7 @@ var _ = Describe("processImagesSpecs", func() {
 	It("image status empty, pull pod exists, nothing to do", func() {
 		gomock.InOrder(
 			micHelper.EXPECT().GetImageState(&testMic, "image 1").Return(kmmv1beta1.ImageState("")),
-			mockPodHelper.EXPECT().getPullPodForImage(pullPods, "image 1").Return(&v1.Pod{}),
+			mockImagePuller.EXPECT().GetPullPodForImage(pullPods, "image 1").Return(&v1.Pod{}),
 		)
 		err := mrh.processImagesSpecs(ctx, &testMic, pullPods)
 		Expect(err).To(BeNil())
@@ -442,190 +429,4 @@ var _ = Describe("processImagesSpecs", func() {
 		Entry("image state ImageNeedsSigning, build/sign config not important, update MBSC to sign action",
 			kmmv1beta1.ImageNeedsSigning, false, false, true, kmmv1beta1.SignImage),
 	)
-})
-
-var _ = Describe("listImagesPullPods", func() {
-	var (
-		ctrl *gomock.Controller
-		clnt *client.MockClient
-		ppm  pullPodManager
-	)
-
-	BeforeEach(func() {
-		ctrl = gomock.NewController(GinkgoT())
-		clnt = client.NewMockClient(ctrl)
-		ppm = newPullPodManager(clnt, scheme)
-	})
-
-	ctx := context.Background()
-	testMic := kmmv1beta1.ModuleImagesConfig{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "some name",
-			Namespace: "some namespace",
-		},
-	}
-
-	It("list succeeded", func() {
-		hl := ctrlclient.HasLabels{imageLabelKey}
-		ml := ctrlclient.MatchingLabels{moduleImageLabelKey: testMic.Name}
-
-		clnt.EXPECT().List(context.Background(), gomock.Any(), ctrlclient.InNamespace(testMic.Namespace), hl, ml).DoAndReturn(
-			func(_ interface{}, podList *v1.PodList, _ ...interface{}) error {
-				podList.Items = []v1.Pod{v1.Pod{}, v1.Pod{}}
-				return nil
-			},
-		)
-
-		pullPods, err := ppm.listImagesPullPods(ctx, &testMic)
-		Expect(err).To(BeNil())
-		Expect(pullPods).ToNot(BeNil())
-	})
-
-	It("list failed", func() {
-		hl := ctrlclient.HasLabels{imageLabelKey}
-		ml := ctrlclient.MatchingLabels{moduleImageLabelKey: testMic.Name}
-
-		clnt.EXPECT().List(context.Background(), gomock.Any(), ctrlclient.InNamespace(testMic.Namespace), hl, ml).Return(fmt.Errorf("some error"))
-
-		pullPods, err := ppm.listImagesPullPods(ctx, &testMic)
-		Expect(err).To(HaveOccurred())
-		Expect(pullPods).To(BeNil())
-	})
-})
-
-var _ = Describe("deletePod", func() {
-	var (
-		ctrl *gomock.Controller
-		clnt *client.MockClient
-		ppm  pullPodManager
-	)
-
-	BeforeEach(func() {
-		ctrl = gomock.NewController(GinkgoT())
-		clnt = client.NewMockClient(ctrl)
-		ppm = newPullPodManager(clnt, scheme)
-	})
-
-	ctx := context.Background()
-
-	It("good flow", func() {
-		pod := v1.Pod{}
-		clnt.EXPECT().Delete(ctx, &pod).Return(nil)
-		err := ppm.deletePod(ctx, &pod)
-		Expect(err).To(BeNil())
-	})
-
-	It("error flow", func() {
-		pod := v1.Pod{}
-		clnt.EXPECT().Delete(ctx, &pod).Return(fmt.Errorf("some error"))
-		err := ppm.deletePod(ctx, &pod)
-		Expect(err).To(HaveOccurred())
-	})
-
-	It("deletion timestamp set", func() {
-		pod := v1.Pod{
-			ObjectMeta: metav1.ObjectMeta{
-				DeletionTimestamp: &metav1.Time{},
-			},
-		}
-		err := ppm.deletePod(ctx, &pod)
-		Expect(err).To(BeNil())
-	})
-})
-
-var _ = Describe("getPullPodForImage", func() {
-	var (
-		ppm pullPodManager
-	)
-
-	BeforeEach(func() {
-		ppm = newPullPodManager(nil, nil)
-	})
-
-	pullPods := []v1.Pod{
-		{
-			ObjectMeta: metav1.ObjectMeta{
-				Labels: map[string]string{imageLabelKey: "image 1"},
-			},
-		},
-		{
-			ObjectMeta: metav1.ObjectMeta{
-				Labels: map[string]string{imageLabelKey: "image 2"},
-			},
-		},
-	}
-
-	It("there is a pull pod for that image", func() {
-		res := ppm.getPullPodForImage(pullPods, "image 2")
-		Expect(res).ToNot(BeNil())
-		Expect(res.Labels[imageLabelKey]).To(Equal("image 2"))
-	})
-
-	It("there is no pull pod for that image", func() {
-		res := ppm.getPullPodForImage(pullPods, "image 23")
-		Expect(res).To(BeNil())
-	})
-})
-
-var _ = Describe("createPullPod", func() {
-	var (
-		ctrl *gomock.Controller
-		clnt *client.MockClient
-		ppm  pullPodManager
-	)
-
-	BeforeEach(func() {
-		ctrl = gomock.NewController(GinkgoT())
-		clnt = client.NewMockClient(ctrl)
-		ppm = newPullPodManager(clnt, scheme)
-	})
-
-	ctx := context.Background()
-	testMic := kmmv1beta1.ModuleImagesConfig{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "some name",
-			Namespace: "some namespace",
-		},
-	}
-	testImageSpec := kmmv1beta1.ModuleImageSpec{
-		Image: "some image",
-	}
-
-	It("check the pod fields", func() {
-		expectedPod := v1.Pod{
-			ObjectMeta: metav1.ObjectMeta{
-				GenerateName: testMic.Name + "-pull-pod-",
-				Namespace:    testMic.Namespace,
-				Labels: map[string]string{
-					moduleImageLabelKey: "some name",
-					imageLabelKey:       "some image",
-				},
-			},
-			Spec: v1.PodSpec{
-				Containers: []v1.Container{
-					{
-						Name:    pullerContainerName,
-						Image:   "some image",
-						Command: []string{"/bin/sh", "-c", "exit 0"},
-					},
-				},
-				RestartPolicy: v1.RestartPolicyOnFailure,
-			},
-		}
-
-		clnt.EXPECT().Create(ctx, gomock.Any()).DoAndReturn(
-			func(ctx context.Context, obj ctrlclient.Object, opts ...ctrlclient.CreateOption) error {
-				if pullPod, ok := obj.(*v1.Pod); ok {
-					pullPod.OwnerReferences = nil
-					if !reflect.DeepEqual(&expectedPod, pullPod) {
-						diff := cmp.Diff(expectedPod, *pullPod)
-						fmt.Println("Differences:\n", diff)
-						return fmt.Errorf("pods not equal")
-					}
-				}
-				return nil
-			})
-		err := ppm.createPullPod(ctx, &testImageSpec, &testMic)
-		Expect(err).To(BeNil())
-	})
 })
