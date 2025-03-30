1491f8572d1c84b3176012b2c3bea38528598369
Yevgeny Shnaidman <yshnaidm@redhat.com>
2025-03-30 10:50:00 +0300

    Passing the MBSC object as an input to buildsign.Manager interface
    Manager interface receives the meta.Object as an input parameter. It
    passes it to SetControllerReference function, which also expects
    meta.Object. But inside the function, the it is cast into runtime.Object
    interface, which requires implementation of DeepCopy function, which is
    implemented by the whole MBSC object

--- internal/controllers/mbsc_reconciler_test.go
+++ internal/controllers/mbsc_reconciler_test.go
@@ -136,12 +136,12 @@ var _ = Describe("updateStatus", func() {
 			},
 		}
 		gomock.InOrder(
-			mockManager.EXPECT().GetStatus(ctx, "some name", "some namespace", "kernel version 1", kmmv1beta1.BuildImage, &testMBSC.ObjectMeta).
+			mockManager.EXPECT().GetStatus(ctx, "some name", "some namespace", "kernel version 1", kmmv1beta1.BuildImage, &testMBSC).
 				Return(kmmv1beta1.ActionSuccess, nil),
 			mockMBSC.EXPECT().SetImageStatus(&testMBSC, "image 1", kmmv1beta1.BuildImage, kmmv1beta1.ActionSuccess),
-			mockManager.EXPECT().GetStatus(ctx, "some name", "some namespace", "kernel version 2", kmmv1beta1.SignImage, &testMBSC.ObjectMeta).
+			mockManager.EXPECT().GetStatus(ctx, "some name", "some namespace", "kernel version 2", kmmv1beta1.SignImage, &testMBSC).
 				Return(kmmv1beta1.BuildOrSignStatus(""), nil),
-			mockManager.EXPECT().GetStatus(ctx, "some name", "some namespace", "kernel version 3", kmmv1beta1.BuildImage, &testMBSC.ObjectMeta).
+			mockManager.EXPECT().GetStatus(ctx, "some name", "some namespace", "kernel version 3", kmmv1beta1.BuildImage, &testMBSC).
 				Return(kmmv1beta1.BuildOrSignStatus(""), fmt.Errorf("some error")),
 			clnt.EXPECT().Status().Return(statusWriter),
 			statusWriter.EXPECT().Patch(ctx, &testMBSC, gomock.Any()).Return(nil),
@@ -203,9 +203,9 @@ var _ = Describe("processImagesSpecs", func() {
 		gomock.InOrder(
 			mockMBSC.EXPECT().GetImageStatus(&testMBSC, "image 1", kmmv1beta1.BuildImage).Return(kmmv1beta1.ActionSuccess),
 			mockMBSC.EXPECT().GetImageStatus(&testMBSC, "image 2", kmmv1beta1.SignImage).Return(kmmv1beta1.ActionFailure),
-			mockManager.EXPECT().Sync(ctx, gomock.Any(), true, kmmv1beta1.SignImage, &testMBSC.ObjectMeta).Return(nil),
+			mockManager.EXPECT().Sync(ctx, gomock.Any(), true, kmmv1beta1.SignImage, &testMBSC).Return(nil),
 			mockMBSC.EXPECT().GetImageStatus(&testMBSC, "image 3", kmmv1beta1.BuildImage).Return(kmmv1beta1.BuildOrSignStatus("")),
-			mockManager.EXPECT().Sync(ctx, gomock.Any(), true, kmmv1beta1.BuildImage, &testMBSC.ObjectMeta).Return(fmt.Errorf("some error")),
+			mockManager.EXPECT().Sync(ctx, gomock.Any(), true, kmmv1beta1.BuildImage, &testMBSC).Return(fmt.Errorf("some error")),
 		)
 
 		err := mrh.processImagesSpecs(ctx, &testMBSC)
@@ -240,15 +240,15 @@ var _ = Describe("garbageCollect", func() {
 
 	It("check all flows", func() {
 		By("garbage collect for build failed")
-		mockManager.EXPECT().GarbageCollect(ctx, "some name", "some namespace", kmmv1beta1.BuildImage, &testMBSC.ObjectMeta).Return(nil, fmt.Errorf("some error"))
+		mockManager.EXPECT().GarbageCollect(ctx, "some name", "some namespace", kmmv1beta1.BuildImage, &testMBSC).Return(nil, fmt.Errorf("some error"))
 		err := mrh.garbageCollect(ctx, &testMBSC)
 		Expect(err).To(HaveOccurred())
 
 		By("garbage collect for sign failed")
 		gomock.InOrder(
-			mockManager.EXPECT().GarbageCollect(ctx, "some name", "some namespace", kmmv1beta1.BuildImage, &testMBSC.ObjectMeta).
+			mockManager.EXPECT().GarbageCollect(ctx, "some name", "some namespace", kmmv1beta1.BuildImage, &testMBSC).
 				Return([]string{}, nil),
-			mockManager.EXPECT().GarbageCollect(ctx, "some name", "some namespace", kmmv1beta1.SignImage, &testMBSC.ObjectMeta).
+			mockManager.EXPECT().GarbageCollect(ctx, "some name", "some namespace", kmmv1beta1.SignImage, &testMBSC).
 				Return(nil, fmt.Errorf("some error")),
 		)
 		err = mrh.garbageCollect(ctx, &testMBSC)
@@ -256,8 +256,8 @@ var _ = Describe("garbageCollect", func() {
 
 		By("success")
 		gomock.InOrder(
-			mockManager.EXPECT().GarbageCollect(ctx, "some name", "some namespace", kmmv1beta1.BuildImage, &testMBSC.ObjectMeta).Return([]string{}, nil),
-			mockManager.EXPECT().GarbageCollect(ctx, "some name", "some namespace", kmmv1beta1.SignImage, &testMBSC.ObjectMeta).Return([]string{}, nil),
+			mockManager.EXPECT().GarbageCollect(ctx, "some name", "some namespace", kmmv1beta1.BuildImage, &testMBSC).Return([]string{}, nil),
+			mockManager.EXPECT().GarbageCollect(ctx, "some name", "some namespace", kmmv1beta1.SignImage, &testMBSC).Return([]string{}, nil),
 		)
 		err = mrh.garbageCollect(ctx, &testMBSC)
 		Expect(err).To(BeNil())
