bf375fb9c68ed7aed24309070968dd072a76b5b7
Yoni Bettan <yonibettan@gmail.com>
2025-04-01 17:27:30 +0300

    Removing `LastTransitionTime` from the NMC status.
    Since we are now checking if a node has rebooted using the node's
    `status.nodeInfo.bootID` then we don't need to compare the node's ready
    time and the NMC last recorded node ready time anymore.
    
    Signed-off-by: Yoni Bettan <yonibettan@gmail.com>

--- internal/controllers/nmc_reconciler.go
+++ internal/controllers/nmc_reconciler.go
@@ -573,12 +573,6 @@ func (h *nmcReconcilerHelperImpl) SyncStatus(ctx context.Context, nmcObj *kmmv1b
 			status.ServiceAccountName = p.Spec.ServiceAccountName
 			status.Tolerations = p.Spec.Tolerations
 
-			podLTT := GetContainerStatus(p.Status.ContainerStatuses, pod.WorkerContainerName).
-				State.
-				Terminated.
-				FinishedAt
-
-			status.LastTransitionTime = podLTT
 			status.BootId = node.Status.NodeInfo.BootID
 
 			nmc.SetModuleStatus(&nmcObj.Status.Modules, *status)
