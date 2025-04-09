21b695fefd3e8ab40ff040ebba6881973bf90d4b
Yoni Bettan <yonibettan@gmail.com>
2025-04-09 14:15:13 +0300

    Refactoring the way we generate volume/volumeMounts in build/sign flows.
    To be used in the build flow as well in the build-sign flow.
    
    Signed-off-by: Yoni Bettan <yonibettan@gmail.com>

--- internal/buildsign/pod/common.go
+++ internal/buildsign/pod/common.go
@@ -12,12 +12,9 @@ import (
 	"github.com/kubernetes-sigs/kernel-module-management/internal/api"
 	"github.com/kubernetes-sigs/kernel-module-management/internal/constants"
 	"github.com/mitchellh/hashstructure/v2"
-	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
 )
-
-const dockerfileVolumeName = "dockerfile"
 
 type TemplateData struct {
 	FilesToSign   []string
@@ -66,17 +63,19 @@ func (bspm *buildSignPodManager) podSpec(mld *api.ModuleLoaderData, containerIma
 		selector = mld.Build.Selector
 	}
 
+	volumes, volumeMounts := makeBuildResourceVolumesAndVolumeMounts(*buildConfig, mld.ImageRepoSecret)
+
 	return v1.PodSpec{
 		Containers: []v1.Container{
 			{
 				Args:         args,
 				Name:         "kaniko",
 				Image:        kanikoImage,
-				VolumeMounts: volumeMounts(mld.ImageRepoSecret, buildConfig),
+				VolumeMounts: volumeMounts,
 			},
 		},
 		RestartPolicy: v1.RestartPolicyNever,
-		Volumes:       volumes(mld.ImageRepoSecret, buildConfig),
+		Volumes:       volumes,
 		NodeSelector:  selector,
 		Tolerations:   mld.Tolerations,
 	}
@@ -114,7 +113,7 @@ func (bspm *buildSignPodManager) containerArgs(mld *api.ModuleLoaderData, destin
 func (bspm *buildSignPodManager) getBuildHashAnnotationValue(ctx context.Context, configMapName, namespace string,
 	podSpec *v1.PodSpec) (uint64, error) {
 
-	dockerfileCM := &corev1.ConfigMap{}
+	dockerfileCM := &v1.ConfigMap{}
 	namespacedName := types.NamespacedName{Name: configMapName, Namespace: namespace}
 	if err := bspm.client.Get(ctx, namespacedName, dockerfileCM); err != nil {
 		return 0, fmt.Errorf("failed to get dockerfile ConfigMap %s: %v", namespacedName, err)
@@ -182,119 +181,4 @@ func (bspm *buildSignPodManager) getSecretData(ctx context.Context, secretName, 
 		return nil, fmt.Errorf("invalid Secret %s format, %s key is missing", namespacedName, secretDataKey)
 	}
 	return data, nil
-}
-
-func volumes(imageRepoSecret *v1.LocalObjectReference, buildConfig *kmmv1beta1.Build) []v1.Volume {
-	volumes := []v1.Volume{dockerfileVolume(dockerfileVolumeName, buildConfig.DockerfileConfigMap)}
-	if imageRepoSecret != nil {
-		volumes = append(volumes, makeImagePullSecretVolume(imageRepoSecret))
-	}
-	volumes = append(volumes, makeBuildSecretVolumes(buildConfig.Secrets)...)
-	return volumes
-}
-
-func volumeMounts(imageRepoSecret *v1.LocalObjectReference, buildConfig *kmmv1beta1.Build) []v1.VolumeMount {
-	volumeMounts := []v1.VolumeMount{dockerfileVolumeMount(dockerfileVolumeName)}
-	if imageRepoSecret != nil {
-		volumeMounts = append(volumeMounts, makeImagePullSecretVolumeMount(imageRepoSecret))
-	}
-	volumeMounts = append(volumeMounts, makeBuildSecretVolumeMounts(buildConfig.Secrets)...)
-	return volumeMounts
-}
-
-func dockerfileVolume(name string, dockerfileConfigMap *v1.LocalObjectReference) v1.Volume {
-	return v1.Volume{
-		Name: name,
-		VolumeSource: v1.VolumeSource{
-			ConfigMap: &v1.ConfigMapVolumeSource{
-				LocalObjectReference: *dockerfileConfigMap,
-				Items: []v1.KeyToPath{
-					{
-						Key:  constants.DockerfileCMKey,
-						Path: "Dockerfile",
-					},
-				},
-			},
-		},
-	}
-}
-
-func dockerfileVolumeMount(name string) v1.VolumeMount {
-	return v1.VolumeMount{
-		Name:      name,
-		ReadOnly:  true,
-		MountPath: "/workspace",
-	}
-}
-
-func makeImagePullSecretVolume(secretRef *v1.LocalObjectReference) v1.Volume {
-	if secretRef == nil {
-		return v1.Volume{}
-	}
-
-	return v1.Volume{
-		Name: volumeNameFromSecretRef(*secretRef),
-		VolumeSource: v1.VolumeSource{
-			Secret: &v1.SecretVolumeSource{
-				SecretName: secretRef.Name,
-				Items: []v1.KeyToPath{
-					{
-						Key:  v1.DockerConfigJsonKey,
-						Path: "config.json",
-					},
-				},
-			},
-		},
-	}
-}
-
-func makeImagePullSecretVolumeMount(secretRef *v1.LocalObjectReference) v1.VolumeMount {
-	if secretRef == nil {
-		return v1.VolumeMount{}
-	}
-
-	return v1.VolumeMount{
-		Name:      volumeNameFromSecretRef(*secretRef),
-		ReadOnly:  true,
-		MountPath: "/kaniko/.docker",
-	}
-}
-
-func makeBuildSecretVolumes(secretRefs []v1.LocalObjectReference) []v1.Volume {
-	volumes := make([]v1.Volume, 0, len(secretRefs))
-
-	for _, secretRef := range secretRefs {
-		vol := v1.Volume{
-			Name: volumeNameFromSecretRef(secretRef),
-			VolumeSource: v1.VolumeSource{
-				Secret: &v1.SecretVolumeSource{
-					SecretName: secretRef.Name,
-				},
-			},
-		}
-
-		volumes = append(volumes, vol)
-	}
-
-	return volumes
-}
-
-func makeBuildSecretVolumeMounts(secretRefs []v1.LocalObjectReference) []v1.VolumeMount {
-	secretVolumeMounts := make([]v1.VolumeMount, 0, len(secretRefs))
-
-	for _, secretRef := range secretRefs {
-		volMount := v1.VolumeMount{
-			Name:      volumeNameFromSecretRef(secretRef),
-			ReadOnly:  true,
-			MountPath: "/run/secrets/" + secretRef.Name,
-		}
-
-		secretVolumeMounts = append(secretVolumeMounts, volMount)
-	}
-
-	return secretVolumeMounts
-}
-
-func volumeNameFromSecretRef(ref v1.LocalObjectReference) string {
-	return "secret-" + ref.Name
 }
---
+++ internal/buildsign/pod/volumes.go
@@ -0,0 +1,190 @@
+package pod
+
+import (
+	"fmt"
+
+	kmmv1beta1 "github.com/kubernetes-sigs/kernel-module-management/api/v1beta1"
+	"github.com/kubernetes-sigs/kernel-module-management/internal/constants"
+	v1 "k8s.io/api/core/v1"
+)
+
+func makeBuildResourceVolumesAndVolumeMounts(buildConfig kmmv1beta1.Build,
+	imageRepoSecret *v1.LocalObjectReference) ([]v1.Volume, []v1.VolumeMount) {
+
+	volumes := []v1.Volume{
+		{
+			Name: dockerfileVolumeName,
+			VolumeSource: v1.VolumeSource{
+				ConfigMap: &v1.ConfigMapVolumeSource{
+					LocalObjectReference: *buildConfig.DockerfileConfigMap,
+					Items: []v1.KeyToPath{
+						{
+							Key:  constants.DockerfileCMKey,
+							Path: "Dockerfile",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, secretRef := range buildConfig.Secrets {
+		vol := v1.Volume{
+			Name: "secret-" + secretRef.Name,
+			VolumeSource: v1.VolumeSource{
+				Secret: &v1.SecretVolumeSource{
+					SecretName: secretRef.Name,
+				},
+			},
+		}
+		volumes = append(volumes, vol)
+	}
+
+	volumeMounts := []v1.VolumeMount{
+		{
+			Name:      dockerfileVolumeName,
+			ReadOnly:  true,
+			MountPath: "/workspace",
+		},
+	}
+
+	for _, secretRef := range buildConfig.Secrets {
+		volMount := v1.VolumeMount{
+			Name:      "secret-" + secretRef.Name,
+			ReadOnly:  true,
+			MountPath: "/run/secrets/" + secretRef.Name,
+		}
+
+		volumeMounts = append(volumeMounts, volMount)
+	}
+
+	if imageRepoSecret != nil {
+
+		volumes = append(volumes,
+			v1.Volume{
+				Name: "secret-" + imageRepoSecret.Name,
+				VolumeSource: v1.VolumeSource{
+					Secret: &v1.SecretVolumeSource{
+						SecretName: imageRepoSecret.Name,
+						Items: []v1.KeyToPath{
+							{
+								Key:  v1.DockerConfigJsonKey,
+								Path: "config.json",
+							},
+						},
+					},
+				},
+			},
+		)
+
+		volumeMounts = append(volumeMounts,
+			v1.VolumeMount{
+				Name:      "secret-" + imageRepoSecret.Name,
+				ReadOnly:  true,
+				MountPath: "/kaniko/.docker",
+			},
+		)
+	}
+
+	return volumes, volumeMounts
+}
+
+func makeSignResourceVolumesAndVolumeMounts(signConfig *kmmv1beta1.Sign,
+	imageRepoSecret *v1.LocalObjectReference) ([]v1.Volume, []v1.VolumeMount) {
+
+	volumes := []v1.Volume{
+		{
+			Name: "secret-" + signConfig.CertSecret.Name,
+			VolumeSource: v1.VolumeSource{
+				Secret: &v1.SecretVolumeSource{
+					SecretName: signConfig.CertSecret.Name,
+					Items: []v1.KeyToPath{
+						{
+							Key:  "cert",
+							Path: "cert.pem",
+						},
+					},
+				},
+			},
+		},
+		{
+			Name: "secret-" + signConfig.KeySecret.Name,
+			VolumeSource: v1.VolumeSource{
+				Secret: &v1.SecretVolumeSource{
+					SecretName: signConfig.KeySecret.Name,
+					Items: []v1.KeyToPath{
+						{
+							Key:  "key",
+							Path: "key.pem",
+						},
+					},
+				},
+			},
+		},
+		{
+			Name: dockerfileVolumeName,
+			VolumeSource: v1.VolumeSource{
+				DownwardAPI: &v1.DownwardAPIVolumeSource{
+					Items: []v1.DownwardAPIVolumeFile{
+						{
+							Path: "Dockerfile",
+							FieldRef: &v1.ObjectFieldSelector{
+								FieldPath: fmt.Sprintf("metadata.annotations['%s']", dockerfileAnnotationKey),
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	volumeMounts := []v1.VolumeMount{
+		{
+			Name:      "secret-" + signConfig.CertSecret.Name,
+			ReadOnly:  true,
+			MountPath: "/run/secrets/cert",
+		},
+		{
+			Name:      "secret-" + signConfig.KeySecret.Name,
+			ReadOnly:  true,
+			MountPath: "/run/secrets/key",
+		},
+		{
+			Name:      dockerfileVolumeName,
+			ReadOnly:  true,
+			MountPath: "/workspace",
+		},
+	}
+
+	if imageRepoSecret != nil {
+
+		volumes = append(
+			volumes,
+			v1.Volume{
+				Name: "secret-" + imageRepoSecret.Name,
+				VolumeSource: v1.VolumeSource{
+					Secret: &v1.SecretVolumeSource{
+						SecretName: imageRepoSecret.Name,
+						Items: []v1.KeyToPath{
+							{
+								Key:  v1.DockerConfigJsonKey,
+								Path: "config.json",
+							},
+						},
+					},
+				},
+			},
+		)
+
+		volumeMounts = append(
+			volumeMounts,
+			v1.VolumeMount{
+				Name:      "secret-" + imageRepoSecret.Name,
+				ReadOnly:  true,
+				MountPath: "/kaniko/.docker",
+			},
+		)
+	}
+
+	return volumes, volumeMounts
+}
