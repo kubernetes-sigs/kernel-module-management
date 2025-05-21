91400e76bed7b31c84e627f540c5b503570601ba
TomerNewman <tnewman@redhat.com>
2025-05-21 15:55:41 +0300

    Making KMM's configuration be persist during upgrade
    Until now, whenever a user upgraded KMM,
    the ConfigMap containing the operator
    configuration was redeployed,
    causing any user changes to be lost.
    This commit removes the ConfigMap from KMM's
    deploy process, meaning KMM will take hard-coded
    default configuration and the user can apply manually the
    ConfigMap, meaning it will not be re-deployed during upgrades.

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,11 +1,20 @@
 package config
 
 import (
+	"bytes"
 	"fmt"
-	"os"
 	"time"
 
+	"github.com/go-logr/logr"
+	"golang.org/x/net/context"
 	"gopkg.in/yaml.v3"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/utils/ptr"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
 	"sigs.k8s.io/controller-runtime/pkg/metrics/server"
@@ -42,37 +51,140 @@ type Config struct {
 	Worker                 Worker         `yaml:"worker"`
 }
 
-func ParseFile(path string) (*Config, error) {
-	fd, err := os.Open(path)
-	if err != nil {
-		return nil, fmt.Errorf("could not open the configuration file: %v", err)
-	}
-	defer fd.Close()
-
-	cfg := Config{}
-
-	if err = yaml.NewDecoder(fd).Decode(&cfg); err != nil {
-		return nil, fmt.Errorf("could not decode configuration file: %v", err)
-	}
-
-	return &cfg, nil
+//go:generate mockgen -source=config.go -package=config -destination=mock_config.go ConfigGetter,configHelperAPI
+type ConfigGetter interface {
+	GetConfig(ctx context.Context, userConfigMapName, userConfigMapNamespace string, isHubConfig bool) (*Config, error)
+	GetManagerOptionsFromConfig(conf *Config, scheme *runtime.Scheme) manager.Options
 }
 
-func (c *Config) ManagerOptions() *manager.Options {
-	metrics := server.Options{
-		BindAddress:   c.Metrics.BindAddress,
-		SecureServing: c.Metrics.SecureServing,
+func NewConfigGetter(logger logr.Logger) ConfigGetter {
+	return &configGetter{
+		configHelper: newConfigHelper(),
+		logger:       logger,
+	}
+}
+
+func (cg *configGetter) GetConfig(ctx context.Context,
+	userConfigMapName, userConfigMapNamespace string,
+	isHubConfig bool) (*Config, error) {
+
+	cfg := cg.configHelper.newDefaultConfig(isHubConfig)
+	clnt, err := cg.configHelper.getClient()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get client %v", err)
 	}
 
-	if c.Metrics.EnableAuthnAuthz {
+	managerConfig := &corev1.ConfigMap{}
+	namespacedName := types.NamespacedName{
+		Namespace: userConfigMapNamespace,
+		Name:      userConfigMapName,
+	}
+	if err := clnt.Get(ctx, namespacedName, managerConfig); err != nil {
+		if errors.IsNotFound(err) {
+			cg.logger.Info("No ConfigMap configuring the manager was found in namespace, using default configuration",
+				"namespace", userConfigMapNamespace, "name", userConfigMapName)
+			return cfg, nil
+		}
+		return nil, fmt.Errorf("failed to get ConfigMap %s/%s: %v", userConfigMapName, userConfigMapNamespace, err)
+	}
+
+	err = cg.configHelper.overrideConfigFromCM(managerConfig, cfg)
+	if err != nil {
+		return nil, fmt.Errorf("unable to load KMM config from ConfigMap %v", err)
+	}
+	return cfg, nil
+}
+
+func (cg *configGetter) GetManagerOptionsFromConfig(conf *Config, scheme *runtime.Scheme) manager.Options {
+
+	metrics := server.Options{
+		BindAddress:   conf.Metrics.BindAddress,
+		SecureServing: conf.Metrics.SecureServing,
+	}
+
+	if conf.Metrics.EnableAuthnAuthz {
 		metrics.FilterProvider = filters.WithAuthenticationAndAuthorization
 	}
 
-	return &manager.Options{
-		HealthProbeBindAddress: c.HealthProbeBindAddress,
-		LeaderElection:         c.LeaderElection.Enabled,
-		LeaderElectionID:       c.LeaderElection.ResourceID,
+	return manager.Options{
+		HealthProbeBindAddress: conf.HealthProbeBindAddress,
+		LeaderElection:         conf.LeaderElection.Enabled,
+		LeaderElectionID:       conf.LeaderElection.ResourceID,
 		Metrics:                metrics,
-		WebhookServer:          webhook.NewServer(webhook.Options{Port: c.WebhookPort}),
+		WebhookServer:          webhook.NewServer(webhook.Options{Port: conf.WebhookPort}),
+		Scheme:                 scheme,
+	}
+}
+
+type configHelperAPI interface {
+	getClient() (client.Client, error)
+	overrideConfigFromCM(cm *corev1.ConfigMap, cfg *Config) error
+	decodeStrictYAMLIntoConfig(yamlData []byte, config *Config) error
+	newDefaultConfig(isHubConfig bool) *Config
+}
+type configGetter struct {
+	configHelper configHelperAPI
+	logger       logr.Logger
+}
+
+type configHelper struct{}
+
+func newConfigHelper() configHelperAPI {
+	return &configHelper{}
+}
+
+func (ch *configHelper) getClient() (client.Client, error) {
+	clnt, err := client.New(ctrl.GetConfigOrDie(), client.Options{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to create new client %v", err)
+	}
+	return clnt, nil
+}
+
+func (ch *configHelper) overrideConfigFromCM(cm *corev1.ConfigMap, cfg *Config) error {
+	const configKey = "controller_config.yaml"
+	if cm == nil {
+		return fmt.Errorf("cm can not be nil")
+	}
+	rawConfig, ok := cm.Data[configKey]
+	if !ok {
+		return fmt.Errorf("key %q not found in ConfigMap %s/%s", configKey, cm.Namespace, cm.Name)
+	}
+	return ch.decodeStrictYAMLIntoConfig([]byte(rawConfig), cfg)
+}
+
+func (ch *configHelper) decodeStrictYAMLIntoConfig(yamlData []byte, config *Config) error {
+	decoder := yaml.NewDecoder(bytes.NewReader(yamlData))
+	decoder.KnownFields(true)
+
+	if err := decoder.Decode(config); err != nil {
+		return fmt.Errorf("error unmarshaling YAML: %v", err)
+	}
+
+	return nil
+}
+
+func (ch *configHelper) newDefaultConfig(isHubConfig bool) *Config {
+	leaderElectionResourceID := "kmm.sigs.x-k8s.io"
+	if isHubConfig {
+		leaderElectionResourceID = "kmm-hub.sigs.x-k8s.io"
+	}
+	return &Config{
+		HealthProbeBindAddress: ":8081",
+		WebhookPort:            9443,
+		LeaderElection: LeaderElection{
+			Enabled:    true,
+			ResourceID: leaderElectionResourceID,
+		},
+		Metrics: Metrics{
+			EnableAuthnAuthz: true,
+			BindAddress:      "0.0.0.0:8443",
+			SecureServing:    true,
+		},
+		Worker: Worker{
+			RunAsUser:        ptr.To[int64](0),
+			SELinuxType:      "spc_t",
+			FirmwareHostPath: ptr.To("/lib/firmware"),
+		},
 	}
 }
