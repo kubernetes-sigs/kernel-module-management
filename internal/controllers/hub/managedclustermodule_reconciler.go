6e94c15a7d35dc0b953253882827fbc32dd3a1e1
TomerNewman <tnewman@redhat.com>
2025-07-14 17:10:03 +0300

    Adding rbac for BuildRun resource
    As part of transitioning to Shipwright,
    We have to add RBAC roles for BuildRun
    resource as BuildRuns will be owned by KMM.
    BuildRuns should have the same roles as pods
    because they will replace them as the default
    build mechanism.

--- internal/controllers/hub/managedclustermodule_reconciler.go
+++ internal/controllers/hub/managedclustermodule_reconciler.go
@@ -67,6 +67,7 @@ type ManagedClusterModuleReconciler struct {
 //+kubebuilder:rbac:groups=kmm.sigs.x-k8s.io,resources=modulebuildsignconfigs/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=work.open-cluster-management.io,resources=manifestworks,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=cluster.open-cluster-management.io,resources=managedclusters,verbs=get;list;watch
+//+kubebuilder:rbac:groups=shipwright.io,resources=buildruns,verbs=list;watch;create;patch;delete
 //+kubebuilder:rbac:groups=core,resources=pods,verbs=create;delete;list;patch;watch
 //+kubebuilder:rbac:groups="core",resources=secrets,verbs=get;list;watch
 //+kubebuilder:rbac:groups="core",resources=configmaps,verbs=get;list;watch
