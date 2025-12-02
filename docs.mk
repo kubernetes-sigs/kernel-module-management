88ab43da4935185c0cc9c18ec14d8385b4ba5f5e
Yoni Bettan <yonibettan@gmail.com>
2025-12-02 12:23:11 +0200

    Bumping the Netlify version for generating docs.
    Signed-off-by: Yoni Bettan <yonibettan@gmail.com>

--- docs.mk
+++ docs.mk
@@ -5,4 +5,4 @@ docs: bin/plantuml.jar
 
 bin/plantuml.jar:
 	mkdir -p bin
-	curl -Lo $@ https://github.com/plantuml/plantuml/releases/download/v1.2023.10/plantuml-1.2023.10.jar
+	curl -Lo $@ https://github.com/plantuml/plantuml/releases/download/v1.2025.10/plantuml-1.2025.10.jar
